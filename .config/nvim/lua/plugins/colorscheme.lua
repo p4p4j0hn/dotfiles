773b3bc4a51837b613f506381be5ce8235055437
John Schmidt <john@schmidthaus.rocks>
2024-10-28 12:08:54 -0700

    Added neovim configuration
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "miikanissi/modus-themes.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("modus-themes").setup({
+        transparent = true,
+      })
+      vim.cmd([[colorscheme modus]]) -- modus_operandi(light), modus_vivendi(dark)
+    end,
+  }
+}
---
+++ .config/nvim/lua/plugins/suda.lua
@@ -0,0 +1,6 @@
+-- Suda allows for editing files with sudo and still use my config!
+return {
+	{
+		"lambdalisue/vim-suda",
+	},
+}
