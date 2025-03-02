d3b7cb4f270d9a83e62d47ecdd7a711e5f548709
John Schmidt <john@schmidthaus.rocks>
2025-03-02 14:03:01 -0800

    tweaks
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- install.sh
+++ install.sh
@@ -38,41 +38,36 @@ fi
 # Install starship if required
 if ! [[ "$(command -v starship)" ]]; then
   curl -sS https://starship.rs/install.sh | sudo sh -s -- -y -b /usr/local/bin
-  if [[ -f /etc/bash.bashrc ]]; then
-    echo 'eval "$(starship init bash)"' | sudo tee -a /etc/bash.bashrc
-  elif [[ -f /etc/bashrc ]]; then
-    echo 'eval "$(starship init bash)"' | sudo tee -a /etc/bashrc
-  fi
 fi
 
 # Install bash-prexec if required
-if ! [[ -f /usr/share/bash-prexec ]]; then
-  sudo curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o /usr/share/bash-prexec
+if ! [[ -f "$HOME/.local/share/bash-preexec.sh" ]]; then
+  curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o "$HOME/.local/share/bash-preexec.sh"
 fi
 
 # Install brew if required
-if ! [[ -d /home/linuxbrew/.linuxbrew ]]; then
-  /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  sudo cp -r $HOME/dotfiles/files/brew/* /etc/profile.d/
-fi
+#if ! [[ -d /home/linuxbrew/.linuxbrew ]]; then
+#  /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#  sudo cp -r $HOME/dotfiles/files/brew/* /etc/profile.d/
+#fi
 
 # source brew environment if it doesn't already exist
-if ! [[ "$(command -v brew)" ]]; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
+#if ! [[ "$(command -v brew)" ]]; then
+#  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+#fi
 
 # Install myrepos and vcsh from brew
-brew install myrepos vcsh
+#brew install myrepos vcsh
 
 clone-dotfiles
 
 # install brew applications
-pushd "$HOME/.config/brew"
-if ! [[ -f brew-init-done ]]; then
-  brew bundle --file common.Brewfile --no-lock
-  touch brew-init-done
-fi
-popd
+#pushd "$HOME/.config/brew"
+#if ! [[ -f brew-init-done ]]; then
+#  brew bundle --file common.Brewfile --no-lock
+#  touch brew-init-done
+#fi
+#popd
 
 # Install tmux plugins
-install-tmux-plugins
+#install-tmux-plugins
