cbff5f0572dd53b9910af3aff839fe044d55de65
John Schmidt <john@schmidthaus.rocks>
2025-05-22 19:42:56 -0700

    Install on debian, remove homebrew and devcontainer stuff
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 set -oeux pipefail
 
+# need to install myrepos and vcsh before running this script
+# NOTE: this needs to be run in a bare shell. ie: it cannot be run
+#       within a tmux session in order to install the tmux plugins
+
+sudo apt update
+
+for PACKAGE in $(cat debian-packages.txt); do
+  if ! sudo apt install -y $PACKAGE; then
+    echo "WARNING: ${PACKAGE} not available"
+  else
+    echo "OK: ${PACKAGE} is installed"
+  fi
+done
+
 clone-dotfiles() {
   # Clone mr branch
   pushd $HOME
@@ -24,50 +38,15 @@ install-tmux-plugins() {
   $HOME/.config/tmux/install_plugins.sh
 }
 
-# if in a devcontainer, just clone the dotfiles
-if [[ -v DEVCONTAINER ]]; then
-  clone-dotfiles
-  install-tmux-plugins
-  echo 'eval "$(starship init bash)"' | sudo tee -a /etc/bash.bashrc
-  mkdir -p $HOME/.ssh
-  chmod 700 $HOME/.ssh
-  echo "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEZn+WQaOkErHh/HdYAigbhaGVpexvzAnUr47T38RnChAAAABHNzaDo= john@schmidthaus.rocks" >$HOME/.ssh/authorized_keys
-  exit
-fi
-
-# Install starship if required
-if ! [[ "$(command -v starship)" ]]; then
-  curl -sS https://starship.rs/install.sh | sudo sh -s -- -y -b /usr/local/bin
-fi
-
 # Install bash-prexec if required
 if ! [[ -f "$HOME/.local/share/bash-preexec.sh" ]]; then
   curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o "$HOME/.local/share/bash-preexec.sh"
 fi
 
-# Install brew if required
-#if ! [[ -d /home/linuxbrew/.linuxbrew ]]; then
-#  /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#  sudo cp -r $HOME/dotfiles/files/brew/* /etc/profile.d/
-#fi
+# Install atuin if required
 
-# source brew environment if it doesn't already exist
-#if ! [[ "$(command -v brew)" ]]; then
-#  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-#fi
-
-# Install myrepos and vcsh from brew
-#brew install myrepos vcsh
 
 clone-dotfiles
 
-# install brew applications
-#pushd "$HOME/.config/brew"
-#if ! [[ -f brew-init-done ]]; then
-#  brew bundle --file common.Brewfile --no-lock
-#  touch brew-init-done
-#fi
-#popd
-
 # Install tmux plugins
-#install-tmux-plugins
+install-tmux-plugins
